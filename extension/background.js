9c414776bf781cdf71a9888a97c3f4f888a417a2
milltanya <tanya.miller.perm@gmail.com>
2021-02-24 22:54:13 +0300

    Get url

--- extension/background.js
+++ extension/background.js
@@ -1,14 +1,56 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-        console.log('The color is green.');
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'developer.chrome.com'},
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+    // chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    //     chrome.declarativeContent.onPageChanged.addRules([{
+    //         conditions: [new chrome.declarativeContent.PageStateMatcher({
+    //             pageUrl: {hostEquals: 'developer.chrome.com'},
+    //         })
+    //         ],
+    //         actions: [new chrome.declarativeContent.ShowPageAction()]
+    //     }]);
+    // });
+
+    chrome.tabs.onActivated.addListener(function(activeInfo) {
+        console.log("tab changed");
+        console.log(activeInfo.tabId);
+        console.log(activeInfo.windowId);
+        // chrome.tabs.query({index: activeInfo.tabId, windowId:activeInfo.windowId}, function(tabs) {
+        chrome.tabs.query({active:true}, function(tabs) {
+            console.log("got tab info");
+            let tab = tabs[0];
+            console.log(tabs);
+            console.log(tab);
+            let tab_url = tab.url;
+            console.log(tab_url);
+
+            let xhr = new XMLHttpRequest();
+            let url = "http://127.0.0.1:8000/api/logs/";
+        
+            xhr.open("POST", url, true); 
+        
+            xhr.setRequestHeader("Content-Type", "application/json"); 
+        
+            xhr.onreadystatechange = function () { 
+                if (xhr.readyState === 4 && xhr.status === 200) { 
+                    console.log('Request sent');
+                    console.log(this.responseText);
+                } 
+            }; 
+        
+    
+            let data = JSON.stringify({ 
+                "url": tab_url,
+                "browser": "Chrome",
+                "start": "2021-02-24 21:13:00",
+                "end": "2021-02-24 21:19:00",
+                "tab_name": "test",
+                "background": false,
+                "user": "tanya"
+            }); 
+            console.log(data);
+        
+            xhr.send(data);
+
+        })
+
     });
 });
--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,30 @@
 let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
-
 changeColor.onclick = function(element) {
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "' + color + '";'});
-    });
+    let xhr = new XMLHttpRequest();
+    let url = "http://127.0.0.1:8000/api/logs/";
+
+    xhr.open("POST", url, true); 
+
+    xhr.setRequestHeader("Content-Type", "application/json"); 
+
+    xhr.onreadystatechange = function () { 
+        if (xhr.readyState === 4 && xhr.status === 200) { 
+            console.log('Request sent');
+            console.log(this.responseText);
+        } 
+    }; 
+
+    let data = JSON.stringify({ 
+        "url": "http://test.url",
+        "browser": "Chrome",
+        "start": "2021-02-24 21:13:00",
+        "end": "2021-02-24 21:19:00",
+        "tab_name": "test",
+        "background": false,
+        "user": "tanya"
+    }); 
+    console.log(data);
+
+    xhr.send(data);
 };
