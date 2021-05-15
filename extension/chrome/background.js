adda0fa2cfc06ddd1a83dcc52a33966f0cc62208
milltanya <tanya.miller.perm@gmail.com>
2021-05-16 00:24:14 +0300

    Fetch meta data

--- extension/chrome/background.js
+++ extension/chrome/background.js
@@ -1,10 +1,28 @@
+function GetPageMetas() {
+    var metas = document.getElementsByTagName('meta'); 
+    var metaArr = [];
+
+    if (metas.length > 0) {
+        for (var i=0; i<metas.length; i++) { 
+            var name = metas[i].getAttribute("name");
+            var property = metas[i].getAttribute("property");
+            var content = metas[i].getAttribute("content");
+            
+            metaArr.push({name, property, content});
+        } 
+    }
+
+    return metaArr;
+}
+
+
 function RegisterTab() {
     chrome.tabs.query({active:true}, function(tabs) {
         let tab = tabs[0];
 
         chrome.storage.local.get(['backendUrl', 'userLoggedIn', 'token'], function(items) {
             if (chrome.runtime.lastError) {
-                console.error('lastError:', chrome.runtime.lastError);
+                console.error('lastError:', chrome.runtime.lastError.message);
                 return;
             }
             if (items.userLoggedIn) {
@@ -14,7 +32,7 @@ function RegisterTab() {
                 fetch(items.backendUrl + '/log/setLastLogEndDateTime', {
                     method: 'POST',
                     headers: {
-                        'Content-Type': 'text/plain',
+                        'Content-Type': 'application/json',
                         'Authorization': 'Bearer ' + items.token
                     },
                     body: datetime
@@ -29,13 +47,13 @@ function RegisterTab() {
                     }
                 }).then((response) => {
                     if (tab.url.startsWith('http://') || tab.url.startsWith('https://')) {
-                        let data = JSON.stringify({ 
+                        let data = { 
                             "url": tab.url,
                             "browser": "Chrome",
                             "startDateTime": datetime,
                             "tabName": tab.title,
                             "background": false
-                        }); 
+                        }; 
                         console.log('data: ', data);
     
                         fetch(items.backendUrl + '/log/add', {
@@ -44,10 +62,58 @@ function RegisterTab() {
                                 'Content-Type': 'application/json',
                                 'Authorization': 'Bearer ' + items.token
                             },
-                            body: data
+                            body: JSON.stringify(data)
                         }).then((response) => {
                             if (response.status == 200) {
                                 console.log('Log added successfully');
+                                response.json().then((logId) => {
+                                    console.log("logId:", logId)
+                                    chrome.scripting.executeScript({
+                                        function: GetPageMetas,
+                                        target: {
+                                            allFrames: true,
+                                            tabId: tab.id
+                                        }
+                                    }).then((injectionResults) => {
+                                        if (chrome.runtime.lastError) {
+                                            console.error('lastError:', chrome.runtime.lastError.message);
+                                        }
+
+                                        metaArr = []
+                                        for (const frameResult of injectionResults) {
+                                            for (const meta of frameResult.result) {
+                                                metaArr.push({
+                                                    name: meta.name, 
+                                                    property: meta.property, 
+                                                    content: meta.content
+                                                });
+                                            }
+                                        }
+
+                                        data = {
+                                            logId: logId,
+                                            metas: metaArr
+                                        };
+                                        console.log("Meta Data:", data);
+                                        fetch(items.backendUrl + '/log/setMetas', {
+                                            method: 'POST',
+                                            headers: {
+                                                'Content-Type': 'application/json',
+                                                'Authorization': 'Bearer ' + items.token
+                                            },
+                                            body: JSON.stringify(data)
+                                        }).then((response) => {
+                                            if (response.status == 200) {
+                                                console.log('Meta added successfully');
+                                            } else if (response.status == 401) {
+                                                console.error('Unauthorized');
+                                                logOut();
+                                            } else {
+                                                console.error('Meta add failure: ', response);
+                                            }
+                                        });
+                                    });
+                                })
                             } else if (response.status == 401) {
                                 console.error('Unauthorized');
                                 logOut();
@@ -71,7 +137,7 @@ function logIn(email, pass) {
     return new Promise((resolve, reject) => {
         chrome.storage.local.get(['backendUrl'], function(items) {
             if (chrome.runtime.lastError) {
-                console.error('lastError:', chrome.runtime.lastError);
+                console.error('lastError:', chrome.runtime.lastError.message);
                 resolve(1);
             }
             fetch(items.backendUrl + '/auth/signin', {
@@ -86,7 +152,7 @@ function logIn(email, pass) {
                     response.json().then((response_data) => {
                         chrome.storage.local.set({ userLoggedIn: true, token: response_data.userInfo.token }, () => {
                             if (chrome.runtime.lastError) {
-                                console.error('lastError:', chrome.runtime.lastError);
+                                console.error('lastError:', chrome.runtime.lastError.message);
                                 resolve(1);
                             }
                             chrome.tabs.onActivated.addListener(RegisterTab);
@@ -106,13 +172,11 @@ function logIn(email, pass) {
 
 function logOut() {
     return new Promise((resolve, reject) => {
-        chrome.storage.local.set({ userLoggedIn: false, token: "" }, () => {
+        chrome.storage.local.set({ userLoggedIn: false, token: null }, () => {
             if (chrome.runtime.lastError) {
-                console.error('lastError:', chrome.runtime.lastError);
+                console.error('lastError:', chrome.runtime.lastError.message);
                 resolve(1);
             } else {
-                chrome.tabs.onActivated.removeListener(RegisterTab);
-                chrome.tabs.onUpdated.removeListener(RegisterTab);
                 console.log('Log out succeessful');
                 resolve(0);
             }
@@ -127,7 +191,9 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     } else if (request.message === 'logout') {
         logOut().then(sendResponse);
         return true;
-    } 
+	} else {
+        console.error("Received unexpected message:", request);
+    }
 });
 
 chrome.runtime.onInstalled.addListener((details) => {
@@ -136,12 +202,12 @@ chrome.runtime.onInstalled.addListener((details) => {
     console.log("backendUrl: ", backendUrl);
     chrome.storage.local.set({ backendUrl: backendUrl }, () => {
         if (chrome.runtime.lastError) {
-            console.error('lastError:', chrome.runtime.lastError);
+            console.error('lastError:', chrome.runtime.lastError.message);
         }
     });
     chrome.storage.local.get(['userLoggedIn'], function(response) {
         if (chrome.runtime.lastError) {
-            console.error('lastError:', chrome.runtime.lastError);
+            console.error('lastError:', chrome.runtime.lastError.message);
         }
         if (response.userLoggedIn) {
             chrome.tabs.onActivated.addListener(RegisterTab);
